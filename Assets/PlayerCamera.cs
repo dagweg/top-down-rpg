6ed9c0a1194096364cdc6d0e06e8bbdd9817a409
DAG WEG <dagtef2@gmail.com>
2024-04-27 21:08:06 +0300

    refactor: modularized the code feat: rotate fixed

--- Assets/PlayerCamera.cs
+++ Assets/PlayerCamera.cs
@@ -17,42 +17,35 @@ public class PlayerCamera : MonoBehaviour
     float mouseX = 0f;
     float mouseY = 0f;
     float accumulatedScroll = 0f; 
-    Vector3 mousePosition;
+
     Vector2 screen;
     Camera mainCamera;
 
     Vector3 move = Vector3.zero;
-    Vector3 cameraMidPoint = Vector3.zero;
-    Vector3 centerWPos = Vector3.zero; // Camera Center World Position
-    Vector3 mouseWPos = Vector3.zero; // Mouse World Position
-    Vector3 cameraStartPosition = Vector3.zero;
-
-    Vector3 camDirectionTowardsMouse = Vector3.zero;
-    Vector3 camDirectionTowardsView = Vector3.zero;
-
+    // Vector3 cameraMidPoint = Vector3.zero;
+    Vector3 cameraInitialPosition = Vector3.zero; 
     void Start()
     {
         mainCamera = Camera.main;
-        cameraStartPosition = transform.position;
+        cameraInitialPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        ScreenInfoPolling();
-        InputPolling();
-        CalculateCameraDirection();
+        GetUserInput();
 
-        CameraMovement();
+        screen = GetScreenDimension();
+
+        HandleCameraMovement();
         
         Debug.Log("Forward: " + Vector3.forward);
     }
 
-    void CameraMovement(){
+    void HandleCameraMovement(){
         
         // The Vector that Determines which Direction the Camera Will Go.
         move = Vector3.zero;
-
         
         if(MouseCanMoveCamera()){
             HandleMouseCameraMovement();
@@ -67,52 +60,77 @@ public class PlayerCamera : MonoBehaviour
         transform.position -= move;
     }
 
-    void InputPolling(){
-        
-        /* Keyboard Movement */
+    void GetUserInput(){        
+        /* Keyboard Input */
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
 
-        /* Mouse Movement */
+        /* Mouse Input */
         mouseX = Input.GetAxis("Mouse X");
         mouseY = Input.GetAxis("Mouse Y");       
-        
     }
 
-    void ScreenInfoPolling(){
-        screen = new Vector2(Screen.width,Screen.height); // Calculate Everytime Incase Of Resize
-        mousePosition = new(Input.mousePosition.x,Input.mousePosition.y,mainCamera.nearClipPlane);
+    Vector2 GetScreenDimension(){
+        return new (Screen.width,Screen.height); // Calculate Everytime Incase Of Resize
+    }
 
+    Vector3 GetCameraWorldPosition(){
         // Get Camera MidPoint
-        cameraMidPoint = new (0.5f,0.5f,mainCamera.nearClipPlane);
-
-        // Get the Camera & Mouse World Space Positions
-        centerWPos = mainCamera.ViewportToWorldPoint(cameraMidPoint);
-        mouseWPos = mainCamera.ScreenToWorldPoint(mousePosition);
+        Vector3 cameraMidPoint = GetCameraMidPoint();
+        
+        // Get the Camera World Space Positions
+        return mainCamera.ViewportToWorldPoint(cameraMidPoint);
     }
 
-    void CalculateCameraDirection(){
-        /* Camera Direction Towards  Mouse*/
-        centerWPos.y = transform.position.y;
-        mouseWPos.y = transform.position.y;
+    Vector3 GetCameraMidPoint(){
+        return new (0.5f,0.5f,mainCamera.nearClipPlane);
+    }
 
-        camDirectionTowardsMouse = (centerWPos - mouseWPos).normalized;
-        camDirectionTowardsMouse.y = 0;
+    Vector3 GetMouseScreenPosition(){
+        return new(Input.mousePosition.x,Input.mousePosition.y,mainCamera.nearClipPlane);
+    }
+
+    Vector3 GetMouseWorldPosition(){
+        Vector3 mouseScreenPosition = new(Input.mousePosition.x,Input.mousePosition.y,mainCamera.nearClipPlane);
+        return mainCamera.ScreenToWorldPoint(mouseScreenPosition);
+    }
+
+    Vector3 GetCameraDirectionToMouse(){
+        Vector3 cameraCenterWorldPosition = GetCameraWorldPosition();
+        Vector3 mouseWorldPosition = GetMouseWorldPosition();
+        cameraCenterWorldPosition.y = transform.position.y; // Clamp the Y component so that it doesn't move when mouse moves
+        mouseWorldPosition.y = transform.position.y; // Clamp the Y component so that it doesn't move when mouse moves
+        /* Camera Direction Towards  Mouse */
+        Vector3 dir = (cameraCenterWorldPosition - mouseWorldPosition).normalized;
+        dir.y = 0;
+        return dir;
+    }
+
+    Vector3 GetCameraDirectionTowardsCenter(){
+        Vector3 camWPos = GetCameraWorldPosition();
+        camWPos.y = transform.position.y;
+        Vector3 dir = (transform.position-GetCameraWorldPosition()).normalized;
+        dir.y = 0;
+        return dir;
     }
 
     void HandleMouseCameraMovement(){
-        move = camDirectionTowardsMouse * cameraSpeed * Time.deltaTime;
+        move = GetCameraDirectionToMouse() * cameraSpeed * Time.deltaTime;
     }
 
     void HandleKeyboardCameraMovement(){
-        move = (Vector3.forward * vertical + Vector3.right * horizontal) * cameraSpeed * Time.deltaTime;
+        Vector3 forwardVector = GetCameraDirectionTowardsCenter();
+        Vector3 rightVector = Vector3.Cross(Vector3.up,forwardVector);
+        move = (forwardVector * vertical + rightVector * horizontal) * cameraSpeed * Time.deltaTime;
     }
 
     void HandleZoomCameraMovement(){
-        Vector3 zoomDirection = (centerWPos - transform.position).normalized * zoomSpeed * Time.deltaTime;
+        
+        Vector3 cameraCenterWorldPosition = GetCameraWorldPosition();
+        Vector3 zoomDirection = (cameraCenterWorldPosition - transform.position).normalized * zoomSpeed * Time.deltaTime;
 
         float rawScroll = Input.mouseScrollDelta.y;
-        accumulatedScroll += rawScroll * zoomSpeed;
+        accumulatedScroll += rawScroll * zoomSpeed; // to add acceleration
 
         if(Mathf.Abs(rawScroll) <= 0.2f){
             accumulatedScroll = 0f; // reset
@@ -124,32 +142,28 @@ public class PlayerCamera : MonoBehaviour
             move += zoomDirection; // Zoom Out
         }
 
-        float zoomMin = cameraStartPosition.y - zoomInDistance;
-        float zoomMax = cameraStartPosition.y + zoomOutDistance;
+        float zoomMin = cameraInitialPosition.y - zoomInDistance;
+        float zoomMax = cameraInitialPosition.y + zoomOutDistance;
         float clampedY = Mathf.Clamp(transform.position.y,zoomMin,zoomMax);
 
         transform.position = new Vector3(transform.position.x,clampedY,transform.position.z);
-
-        // Debug.Log("Zoom" + zoomDirection);
     }
 
     void HandleRotateCameraMovement(){
         float rotationMagnitude = mouseX * Time.deltaTime * rotateSpeed;
-        // Debug.Log("Rotation Magnitude" + rotationMagnitude);
+        Vector3 cameraWorldPosition = GetCameraWorldPosition();
         if(Input.GetKey(KeyCode.R)){
-            transform.RotateAround(centerWPos, Vector3.up, rotationMagnitude);
-            CalculateCameraDirection();
+            transform.RotateAround(cameraWorldPosition, Vector3.up, rotationMagnitude);
         }
-    
     }
-
     
     bool MouseCanMoveCamera(){
-        if (mousePosition.x < 0 || mousePosition.y < 0 || mousePosition.x > screen.x || mousePosition.y > screen.y) return false;
+        Vector3 mouseSPos = GetMouseScreenPosition();
+        if (mouseSPos.x < 0 || mouseSPos.y < 0 || mouseSPos.x > screen.x || mouseSPos.y > screen.y) return false;
 
-        return  mousePosition.y <= screenSafeZone.y ||   
-                mousePosition.y >= screen.y - screenSafeZone.y || 
-                mousePosition.x <= screenSafeZone.x || 
-                mousePosition.x >= screen.x - screenSafeZone.x;
+        return  mouseSPos.y <= screenSafeZone.y ||   
+                mouseSPos.y >= screen.y - screenSafeZone.y || 
+                mouseSPos.x <= screenSafeZone.x || 
+                mouseSPos.x >= screen.x - screenSafeZone.x;
     }
 }
